30d2f5e7ef2fd997d4813015e5a806dbc9c81fc4
Julien Capellari <julien.capellari@neoxia.com>
2022-09-12 12:16:20 +0200

    chore: setup serverless on backend

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,35 +1,79 @@
-var ZipPlugin = require('zip-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('node:path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
 
-module.exports = (options, webpack) => {
-  const lazyImports = [
-    '@nestjs/microservices/microservices-module',
-    '@nestjs/websockets/socket-module',
-  ];
+const lazyImports = [
+  '@nestjs/microservices/microservices-module',
+  '@nestjs/websockets/socket-module',
+  '@nestjs/microservices',
+  'cache-manager',
+  'class-validator',
+  'class-transformer',
+];
 
-  return {
-    ...options,
-    externals: [],
-    output: {
-      ...options.output,
-      libraryTarget: 'commonjs2'
-    },
-    plugins: [
-      ...options.plugins,
-      new webpack.IgnorePlugin({
-        checkResource(resource) {
-          if (lazyImports.includes(resource)) {
-            try {
-              require.resolve(resource);
-            } catch (err) {
-              return true;
+module.exports = {
+  entry: slsw.lib.entries,
+  devtool: slsw.lib.webpack.isLocal ? 'inline-source-map' : false,
+  target: 'node',
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  // externals: [nodeExternals()],
+  externalsPresets: { node: true },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              configFile: 'tsconfig.build.json'
             }
           }
-          return false;
-        },
-      }),
-      new ZipPlugin({
-        filename: 'lambda.zip'
-      })
-    ],
-  };
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    // plugins: [
+    //   new TsconfigPathsPlugin({
+    //     configFile: tsConfigFile,
+    //   }),
+    // ],
+  },
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    nodeEnv: false,
+  },
+  node: {
+    __filename: false,
+    __dirname: false,
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      checkResource(resource) {
+        if (lazyImports.includes(resource)) {
+          try {
+            require.resolve(resource);
+          } catch (err) {
+            return true;
+          }
+        }
+        return false;
+      },
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        configFile: 'tsconfig.build.json'
+      }
+    })
+  ],
 };
